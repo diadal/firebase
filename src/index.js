ae02350b610497a0406a49f8945e21160c26cb14
Diadal <info@diadal.com.ng>
2022-03-22 23:40:43 -0700

    update

--- src/index.js
+++ src/index.js
@@ -6,31 +6,33 @@
  * API: https://github.com/quasarframework/quasar/blob/master/app/lib/app-extension/IndexAPI.js
  */
 
-const path = require('path');
+const path = require("path");
 
 const extendWebpackForWeb2 = function (appDir, conf) {
   const userSWFilePath = path.join(
     appDir,
-    'src/assets',
-    'firebase-messaging-sw.js'
+    "src/assets",
+    "firebase-messaging-sw.js"
   );
 
-  const fireSw =  { import: userSWFilePath, filename: 'firebase-messaging-sw.js' };
-  const postentry = {...conf.entry, fireSw}
-  conf.entry = postentry
-
-}
+  const fireSw = {
+    import: userSWFilePath,
+    filename: "firebase-messaging-sw.js",
+  };
+  const postentry = { ...conf.entry, fireSw };
+  conf.entry = postentry;
+};
 
 module.exports = function (api) {
-  api.compatibleWith('@quasar/app', '^1.0.0 || ^2.0.0 || ^3.0.0');
+  api.compatibleWith(
+    "@quasar/app",
+    "^1.0.0 || ^2.0.0 || ^3.0.0 || v3.0.0-rc.3"
+  );
 
   const modeName = api.ctx.modeName;
   const appDir = api.appDir;
 
-  if (['pwa', 'spa', 'ssr'].includes(modeName)) {
+  if (["pwa", "spa", "ssr"].includes(modeName)) {
     api.extendWebpack((conf) => extendWebpackForWeb2(appDir, conf));
-
   }
-
-
 };
